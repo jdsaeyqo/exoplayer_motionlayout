79b31c268819ee70aa4e38f2e91d70f622602ffd
Sang <jdsaeyqi@naver.com>
2021-07-20 15:55:33 +0900

    리사이클러뷰, 뷰바인딩 적용

--- app/src/main/java/com/example/exoplayer/adapter/VideoAdapter.kt
+++ app/src/main/java/com/example/exoplayer/adapter/VideoAdapter.kt
@@ -1,27 +1,23 @@
 package com.example.exoplayer.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.exoplayer.R
+import com.example.exoplayer.databinding.ItemVideoBinding
 import com.example.exoplayer.model.VideoModel
 
 
-class VideoAdapter(val callback : (String,String) -> Unit) : ListAdapter<VideoModel, VideoAdapter.ViewHolder>(diffUtil) {
+class VideoAdapter(val callback: (String, String) -> Unit) :
+    RecyclerView.Adapter<VideoAdapter.ViewHolder>() {
 
-    inner class ViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
+    private var videoList: List<VideoModel> = listOf()
 
-        fun bind(item : VideoModel){
 
-            val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
-            val subTitleTextView = view.findViewById<TextView>(R.id.subTitleTextView)
-            val thumbnailImageView = view.findViewById<ImageView>(R.id.thumbnailImageView)
+    inner class ViewHolder(private val binding: ItemVideoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: VideoModel) = with(binding) {
 
             titleTextView.text = item.title
             subTitleTextView.text = item.subtitle
@@ -30,31 +26,30 @@ class VideoAdapter(val callback : (String,String) -> Unit) : ListAdapter<VideoMo
                 .load(item.thumb)
                 .into(thumbnailImageView)
 
-            view.setOnClickListener {
-            callback(item.sources,item.title)
+            root.setOnClickListener {
+                callback(item.sources, item.title)
             }
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_video,parent,false))
+
+        val view = ItemVideoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        return holder.bind(currentList[position])
+
+        holder.bind(videoList[position])
     }
 
-    companion object{
-        val diffUtil = object :DiffUtil.ItemCallback<VideoModel>(){
-            override fun areItemsTheSame(oldItem: VideoModel, newItem: VideoModel): Boolean {
-                return oldItem == newItem
-            }
+    override fun getItemCount(): Int = videoList.size
 
-            override fun areContentsTheSame(oldItem: VideoModel, newItem: VideoModel): Boolean {
-                return oldItem == newItem
-            }
-
-        }
+    fun setList(videoList: List<VideoModel>) {
+        this.videoList = videoList
+        notifyDataSetChanged()
     }
+
+
 }
